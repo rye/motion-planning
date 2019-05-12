fac06e61661de214cc0aeca1c2811622eb956986
Kristofer Rye <kristofer.rye@gmail.com>
2019-05-11 22:12:58 -0500

    Add basic straight-line example
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/bin/mp.rs
+++ src/bin/mp.rs
@@ -1,3 +1,28 @@
+extern crate motion_planning;
+
+use motion_planning::{Pose, Segment, Vec3d};
+
 fn main() {
-	println!("Hello, world!");
+	let segment: Segment<Pose<Vec3d<f64>>> = Segment {
+		start: Pose {
+			position: (0.0, 0.0, 0.0).into(),
+			velocity: (0.0, 0.0, 0.0).into(),
+			acceleration: (0.0, 0.0, 0.0).into(),
+		},
+		end: Pose {
+			position: (0.0, 1.0, 0.0).into(),
+			velocity: (0.0, 0.0, 0.0).into(),
+			acceleration: (0.0, 0.0, 0.0).into(),
+		},
+	};
+
+	for t in 0..1000 {
+		let t: f64 = t as f64 * 0.001;
+		let pos = segment.position_at(t);
+
+		println!("{},{},{},{}", t, pos.x, pos.y, pos.z);
+
+	}
+
+	//println!("Hello, world!");
 }
