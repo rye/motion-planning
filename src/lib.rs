0e65814a5fd9031db9efab5190e3e891e0ab2c2c
Kristofer Rye <kristofer.rye@gmail.com>
2019-05-13 13:15:12 -0500

    Generalize and clean up Segment structure
    I realized that when defining structures of higher length, we would end
    up with structures that duplicate information.  Instead, we use a
    general form for "path" structures---a Vec.  The advantage of doing
    this is that we just need to run a few small computations to get this
    working.
    
    Vec's are assumed to be in the order of traversal, so care must be
    taken to ensure that points are in order.  (BTreeMap would sort points
    by coordinate, and would therefore be incorrect.)
    
    Also, position_at() calculations now return an Option.  Most of the
    time this will be Some(value), but in cases where the length of a Path
    is strictly zero, this value is None.  This is not an Err condition,
    just a confusing one.
    
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,22 @@
 trait Position {}
 
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Vec3d<V>(pub V, pub V, pub V);
 
-impl<T: std::ops::Mul<T> + Copy> std::ops::Mul<T> for &Vec3d<T>
+impl<T, V> std::ops::Mul<T> for Vec3d<V>
 where
-	T: std::convert::From<<T as std::ops::Mul>::Output>,
+	T: std::ops::Mul<T, Output = T>,
+	T: std::convert::From<V>,
+	T: Copy,
 {
 	type Output = Vec3d<T>;
 
 	fn mul(self, scalar: T) -> Vec3d<T> {
-		Vec3d {
-			0: (self.0 * scalar).into(),
-			1: (self.1 * scalar).into(),
-			2: (self.2 * scalar).into(),
-		}
+		Vec3d(
+			scalar * self.0.into(),
+			scalar * self.1.into(),
+			scalar * self.2.into(),
+		)
 	}
 }
 
@@ -39,20 +41,37 @@ pub struct Pose<V> {
 	pub acceleration: V,
 }
 
-pub struct Segment<V> {
-	pub start: V,
-	pub end: V,
+pub trait Path<V> {
+	fn position_at(&self, t: f64) -> Option<V>;
+	fn velocity_at(&self, t: f64) -> Option<V>;
 }
 
-impl Segment<Pose<Vec3d<f64>>> {
-	pub fn position_at(&self, t: f64) -> Vec3d<f64> {
-		let p0 = &self.start.position;
-		let v0 = &self.start.velocity;
-		let a0 = &self.start.acceleration;
+impl<V> Path<V> for std::vec::Vec<Pose<V>>
+where
+	V: Copy,
+	V: std::ops::Mul<f64, Output = V>,
+	V: std::ops::Add<V, Output = V>,
+{
+	fn position_at(&self, t: f64) -> Option<V> {
+		let length = self.len();
 
-		let p1 = &self.end.position;
-		let v1 = &self.end.velocity;
-		let a1 = &self.end.acceleration;
+		if let 0 = length {
+			return None;
+		}
+
+		let prec_idx = (t / length as f64).floor() as usize;
+		let succ_idx = (t / length as f64).ceil() as usize;
+
+		let prec: &Pose<V> = &self[prec_idx];
+		let succ: &Pose<V> = &self[succ_idx];
+
+		let p0 = prec.position;
+		let v0 = prec.velocity;
+		let a0 = prec.acceleration;
+
+		let p1 = succ.position;
+		let v1 = succ.velocity;
+		let a1 = succ.acceleration;
 
 		let t1 = t;
 		let t2 = t.powi(2);
@@ -67,7 +86,11 @@ impl Segment<Pose<Vec3d<f64>>> {
 		let h45 = -4. * t3 + 7. * t4 - 3. * t5;
 		let h55 = 10. * t3 - 15. * t4 + 6. * t5;
 
-		(p0 * h05) + (v0 * h15) + (a0 * h25) + (a1 * h35) + (v1 * h45) + (p1 * h55)
+		Some((p0 * h05) + (v0 * h15) + (a0 * h25) + (a1 * h35) + (v1 * h45) + (p1 * h55))
+	}
+
+	fn velocity_at(&self, _t: f64) -> Option<V> {
+		unimplemented!();
 	}
 }
 
@@ -76,24 +99,62 @@ mod tests {
 	use super::*;
 
 	#[test]
-	fn hermite_works_in_a_straight_line() {
-		let segment: Segment<Pose<Vec3d<f64>>> = Segment {
-			start: Pose {
-				position: Vec3d(0.0, 0.0, 0.0),
-				velocity: Vec3d(0.0, 0.0, 0.0),
-				acceleration: Vec3d(0.0, 0.0, 0.0),
-			},
-			end: Pose {
-				position: Vec3d(0.0, 1.0, 0.0),
-				velocity: Vec3d(0.0, 0.0, 0.0),
-				acceleration: Vec3d(0.0, 0.0, 0.0),
-			},
-		};
+	fn vec_addition() {
+		let a: Vec3d<f32> = Vec3d(1.0, 2.0, 3.0);
+		let b: Vec3d<f32> = Vec3d(5.0, 4.0, 3.0);
 
-		assert_eq!(segment.position_at(0.0), segment.start.position);
-		assert_eq!(segment.position_at(0.25), Vec3d(0.0, 0.103515625, 0.0));
-		assert_eq!(segment.position_at(0.5), Vec3d(0.0, 0.5, 0.0));
-		assert_eq!(segment.position_at(0.75), Vec3d(0.0, 0.896484375, 0.0));
-		assert_eq!(segment.position_at(1.0), segment.end.position);
+		assert_eq!(a + b, Vec3d(6., 6., 6.));
+	}
+
+	#[test]
+	fn vec_scalar_multiplication_f64() {
+		let a: Vec3d<f64> = Vec3d(1.0, 2.0, 3.0);
+		let b: f64 = 2.0;
+
+		assert_eq!(a * b, Vec3d(2.0f64, 4.0f64, 6.0f64));
+	}
+
+	#[test]
+	fn vec_scalar_multiplication_f32() {
+		let a: Vec3d<f32> = Vec3d(1.0, 2.0, 3.0);
+		let b: f32 = 2.0;
+
+		assert_eq!(a * b, Vec3d(2.0f32, 4.0f32, 6.0f32));
+	}
+
+	#[test]
+	fn vec_scalar_multiplication_vf32_sf64() {
+		let a: Vec3d<f32> = Vec3d(1.0, 2.0, 3.0);
+		let b: f64 = 2.0;
+
+		assert_eq!(a * b, Vec3d(2.0f64, 4.0f64, 6.0f64));
+	}
+
+	#[test]
+	fn hermite_works_in_a_straight_line() {
+		let mut segment = Vec::new();
+
+		segment.push(Pose {
+			position: Vec3d(0.0f64, 0.0, 0.0),
+			velocity: Vec3d(0.0, 0.0, 0.0),
+			acceleration: Vec3d(0.0, 0.0, 0.0),
+		});
+
+		segment.push(Pose {
+			position: Vec3d(0.0f64, 1.0, 0.0),
+			velocity: Vec3d(0.0, 0.0, 0.0),
+			acceleration: Vec3d(0.0, 0.0, 0.0),
+		});
+
+		assert_eq!(segment.position_at(0.0), Some(segment[0].position));
+		assert_eq!(segment.position_at(0.5), Some(Vec3d(0.0, 0.5, 0.0)));
+		assert_eq!(segment.position_at(1.0), Some(segment[1].position));
+	}
+
+	#[test]
+	fn position_length_zero() {
+		let segment: Vec<Pose<Vec3d<f64>>> = Vec::new();
+
+		assert_eq!(segment.position_at(0.0), None);
 	}
 }
