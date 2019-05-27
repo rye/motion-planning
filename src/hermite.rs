05b66bb9e54c1f70f3b82149243e2f1a483a7c95
Kristofer Rye <kristofer.rye@gmail.com>
2019-05-27 17:59:15 -0500

    Add benchmarks for quintic hermite coefficient helpers
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/hermite.rs
+++ src/hermite.rs
@@ -1,3 +1,10 @@
+#[cfg(test)]
+extern crate rand;
+#[cfg(test)]
+extern crate test;
+#[cfg(test)]
+use rand::Rng;
+
 pub fn h_5(t: f64, n: usize) -> f64 {
 	let t2 = t.powi(2);
 	let t3 = t.powi(3);
@@ -48,6 +55,60 @@ fn h_5_is_correct() {
 	assert_eq!(h_5(1.0, 5), 1.);
 }
 
+#[bench]
+fn h_5_0_1_0(b: &mut test::Bencher) {
+	let mut rng = rand::thread_rng();
+
+	b.iter(|| {
+		test::black_box(h_5(rng.gen(), 0));
+	})
+}
+
+#[bench]
+fn h_5_0_1_1(b: &mut test::Bencher) {
+	let mut rng = rand::thread_rng();
+
+	b.iter(|| {
+		test::black_box(h_5(rng.gen(), 1));
+	})
+}
+
+#[bench]
+fn h_5_0_1_2(b: &mut test::Bencher) {
+	let mut rng = rand::thread_rng();
+
+	b.iter(|| {
+		test::black_box(h_5(rng.gen(), 2));
+	})
+}
+
+#[bench]
+fn h_5_0_1_3(b: &mut test::Bencher) {
+	let mut rng = rand::thread_rng();
+
+	b.iter(|| {
+		test::black_box(h_5(rng.gen(), 3));
+	})
+}
+
+#[bench]
+fn h_5_0_1_4(b: &mut test::Bencher) {
+	let mut rng = rand::thread_rng();
+
+	b.iter(|| {
+		test::black_box(h_5(rng.gen(), 4));
+	})
+}
+
+#[bench]
+fn h_5_0_1_5(b: &mut test::Bencher) {
+	let mut rng = rand::thread_rng();
+
+	b.iter(|| {
+		test::black_box(h_5(rng.gen(), 5));
+	})
+}
+
 #[test]
 fn h_5p_is_correct() {
 	assert_eq!(h_5p(0.0, 0), 0.);
@@ -63,4 +124,58 @@ fn h_5p_is_correct() {
 	assert_eq!(h_5p(1.0, 3), 0.);
 	assert_eq!(h_5p(1.0, 4), 1.);
 	assert_eq!(h_5p(1.0, 5), 0.);
+}
+
+#[bench]
+fn h_5p_0_1_0(b: &mut test::Bencher) {
+	let mut rng = rand::thread_rng();
+
+	b.iter(|| {
+		test::black_box(h_5p(rng.gen(), 0));
+	})
+}
+
+#[bench]
+fn h_5p_0_1_1(b: &mut test::Bencher) {
+	let mut rng = rand::thread_rng();
+
+	b.iter(|| {
+		test::black_box(h_5p(rng.gen(), 1));
+	})
+}
+
+#[bench]
+fn h_5p_0_1_2(b: &mut test::Bencher) {
+	let mut rng = rand::thread_rng();
+
+	b.iter(|| {
+		test::black_box(h_5p(rng.gen(), 2));
+	})
+}
+
+#[bench]
+fn h_5p_0_1_3(b: &mut test::Bencher) {
+	let mut rng = rand::thread_rng();
+
+	b.iter(|| {
+		test::black_box(h_5p(rng.gen(), 3));
+	})
+}
+
+#[bench]
+fn h_5p_0_1_4(b: &mut test::Bencher) {
+	let mut rng = rand::thread_rng();
+
+	b.iter(|| {
+		test::black_box(h_5p(rng.gen(), 4));
+	})
+}
+
+#[bench]
+fn h_5p_0_1_5(b: &mut test::Bencher) {
+	let mut rng = rand::thread_rng();
+
+	b.iter(|| {
+		test::black_box(h_5p(rng.gen(), 5));
+	})
 }
