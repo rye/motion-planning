7efa9c475c9ed2faaf8c4c2b8ca90d4029f4da96
Kristofer Rye <kristofer.rye@gmail.com>
2019-05-28 11:39:04 -0500

    Add second-derivative Hermite basis functions
    As you might expect, this is supposed to satisfy the following
    constraint:
    
      H_x^5''(0, x) == 1 iff x == 2, and
      H_x^5''(1, x) == 1 iff x == 4.
    
    Note that I can't keep going with this---quintic hermite basis
    functions only work up to acceleration.  The next derivative, for
    example, would have H_x^5''(0, 0) == -60, which is wrong.  To get
    higher degrees, I need to develop septic Hermite basis functions.
    
    This works for most motion planning, so I'm calling it here.
    
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/hermite.rs
+++ src/hermite.rs
@@ -31,6 +31,21 @@ pub fn h_5p(t: f64, n: usize) -> f64 {
 	}
 }
 
+pub fn h_5pp(t: f64, n: usize) -> f64 {
+	let t2 = t.powi(2);
+	let t3 = t.powi(3);
+
+	match n {
+		0 => -60. * t + 180. * t2 - 120. * t3,
+		1 => -36. * t + 96. * t2 - 60. * t3,
+		2 => 1. - 9. * t + 18. * t2 - 10. * t3,
+		3 => 3. * t - 12. * t2 + 10. * t3,
+		4 => -24. * t + 84. * t2 - 60. * t3,
+		5 => 60. * t - 180. * t2 + 120. * t3,
+		_ => unimplemented!(),
+	}
+}
+
 #[test]
 fn h_5_is_correct() {
 	assert_eq!(h_5(0.0, 0), 1.);
@@ -63,4 +78,21 @@ fn h_5p_is_correct() {
 	assert_eq!(h_5p(1.0, 3), 0.);
 	assert_eq!(h_5p(1.0, 4), 1.);
 	assert_eq!(h_5p(1.0, 5), 0.);
+}
+
+#[test]
+fn h_5pp_is_correct() {
+	assert_eq!(h_5pp(0.0, 0), 0.);
+	assert_eq!(h_5pp(0.0, 1), 0.);
+	assert_eq!(h_5pp(0.0, 2), 1.);
+	assert_eq!(h_5pp(0.0, 3), 0.);
+	assert_eq!(h_5pp(0.0, 4), 0.);
+	assert_eq!(h_5pp(0.0, 5), 0.);
+
+	assert_eq!(h_5pp(1.0, 0), 0.);
+	assert_eq!(h_5pp(1.0, 1), 0.);
+	assert_eq!(h_5pp(1.0, 2), 0.);
+	assert_eq!(h_5pp(1.0, 3), 1.);
+	assert_eq!(h_5pp(1.0, 4), 0.);
+	assert_eq!(h_5pp(1.0, 5), 0.);
 }
