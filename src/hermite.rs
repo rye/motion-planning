b03b7e6bb44735ce72e0a81aa0bf702474d82236
Kristofer Rye <kristofer.rye@gmail.com>
2019-05-29 15:21:59 -0500

    Document the h_5, h_5p, and h_5pp methods
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/hermite.rs
+++ src/hermite.rs
@@ -1,3 +1,62 @@
+/// Computes the value of a quintic Hermite basis function.
+///
+/// The coefficients are results from the following Mathematica code.  In
+/// layman's terms, the reason these basis functions are significant is that,
+/// depending on the value of `n` and `t`, they range between `0` and `1`, in a
+/// manner that is smooth.  `c[t]`, in the below code, is a standard quintic
+/// polynomial, so the code below simply solves for values of `b0` through `b5`
+/// and then rewrites `c[t]` in that form before rearranging.
+///
+/// ```Mathematica
+/// c[t_] := b0 + b1 t + b2 t^2 + b3 t^3 + b4 t^4 + b5 t^5;
+/// Collect[
+///   c[t] /.
+///     Solve[
+///       {
+///         (c[t] /. t -> 0) == p0,
+///         (D[c[t], t] /. t -> 0) == v0,
+///         (D[D[c[t], t], t] /. t -> 0) == a0,
+///         (D[D[c[t], t], t] /. t -> 1) == a1
+///         (D[c[t], t] /. t -> 1) == v1,
+///         (c[t] /. t -> 1) == p1
+///       },
+///       {b0, b1, b2, b3, b4, b5}
+///     ],
+///   {p0, v0, a0, a1, v1, p1}
+/// ]
+/// ```
+///
+/// # Examples
+///
+/// ```rust
+/// use motion_planning::hermite::h_5;
+///
+/// // at t=0, all of h_n^5 are `0` except for `h_0^5`.
+/// assert_eq!(h_5(0., 0), 1.);
+/// assert_eq!(h_5(0., 1), 0.);
+/// assert_eq!(h_5(0., 2), 0.);
+/// assert_eq!(h_5(0., 3), 0.);
+/// assert_eq!(h_5(0., 4), 0.);
+/// assert_eq!(h_5(0., 5), 0.);
+///
+/// // at t=1, all of h_n^5 are `0` except for `h_5^5`.
+/// assert_eq!(h_5(1., 0), 0.);
+/// assert_eq!(h_5(1., 1), 0.);
+/// assert_eq!(h_5(1., 2), 0.);
+/// assert_eq!(h_5(1., 3), 0.);
+/// assert_eq!(h_5(1., 4), 0.);
+/// assert_eq!(h_5(1., 5), 1.);
+/// ```
+///
+/// # Panics
+///
+/// If `n` is not one of `0`, `1`, `2`, `3`, `4`, or `5`, this function panics.
+///
+/// ```should_panic
+/// use motion_planning::hermite::h_5;
+/// # let t = 0.5_f64;
+/// h_5(t, 7);
+/// ```
 pub fn h_5(t: f64, n: usize) -> f64 {
 	let t2 = t.powi(2);
 	let t3 = t.powi(3);
@@ -15,6 +74,43 @@ pub fn h_5(t: f64, n: usize) -> f64 {
 	}
 }
 
+/// Computes the value of the first time-derivative of a quintic Hermite basis
+/// function.
+///
+/// In other words, this function is the first time-derivative of the
+/// [`h_5`](crate::hermite::h_5) function.
+///
+/// # Examples
+///
+/// ```rust
+/// use motion_planning::hermite::h_5p;
+///
+/// // at t=0, all of h_n^5' are `0` except for `h_1^5'`.
+/// assert_eq!(h_5p(0., 0), 0.);
+/// assert_eq!(h_5p(0., 1), 1.);
+/// assert_eq!(h_5p(0., 2), 0.);
+/// assert_eq!(h_5p(0., 3), 0.);
+/// assert_eq!(h_5p(0., 4), 0.);
+/// assert_eq!(h_5p(0., 5), 0.);
+///
+/// // at t=1, all of h_n^5' are `0` except for `h_4^5'`.
+/// assert_eq!(h_5p(1., 0), 0.);
+/// assert_eq!(h_5p(1., 1), 0.);
+/// assert_eq!(h_5p(1., 2), 0.);
+/// assert_eq!(h_5p(1., 3), 0.);
+/// assert_eq!(h_5p(1., 4), 1.);
+/// assert_eq!(h_5p(1., 5), 0.);
+/// ```
+///
+/// # Panics
+///
+/// If `n` is not one of `0`, `1`, `2`, `3`, `4`, or `5`, this function panics.
+///
+/// ```should_panic
+/// use motion_planning::hermite::h_5p;
+/// # let t = 0.5_f64;
+/// h_5p(t, 7);
+/// ```
 pub fn h_5p(t: f64, n: usize) -> f64 {
 	let t2 = t.powi(2);
 	let t3 = t.powi(3);
@@ -31,6 +127,44 @@ pub fn h_5p(t: f64, n: usize) -> f64 {
 	}
 }
 
+/// Computes the value of the second time-derivative of a quintic Hermite basis
+/// function.
+///
+/// In other words, this function is the second time-derivative of the
+/// [`h_5`](crate::hermite::h_5) function, or equivalently the first
+/// time-derivative of the [`h_5p`](crate::hermite::h_5p) function.
+///
+/// # Examples
+///
+/// ```rust
+/// use motion_planning::hermite::h_5pp;
+///
+/// // at t=0, all of h_n^5'' are `0` except for `h_2^5''`.
+/// assert_eq!(h_5pp(0., 0), 0.);
+/// assert_eq!(h_5pp(0., 1), 0.);
+/// assert_eq!(h_5pp(0., 2), 1.);
+/// assert_eq!(h_5pp(0., 3), 0.);
+/// assert_eq!(h_5pp(0., 4), 0.);
+/// assert_eq!(h_5pp(0., 5), 0.);
+///
+/// // at t=1, all of h_n^5'' are `0` except for `h_3^5''`.
+/// assert_eq!(h_5pp(1., 0), 0.);
+/// assert_eq!(h_5pp(1., 1), 0.);
+/// assert_eq!(h_5pp(1., 2), 0.);
+/// assert_eq!(h_5pp(1., 3), 1.);
+/// assert_eq!(h_5pp(1., 4), 0.);
+/// assert_eq!(h_5pp(1., 5), 0.);
+/// ```
+///
+/// # Panics
+///
+/// If `n` is not one of `0`, `1`, `2`, `3`, `4`, or `5`, this function panics.
+///
+/// ```should_panic
+/// use motion_planning::hermite::h_5pp;
+/// # let t = 0.5_f64;
+/// h_5pp(t, 7);
+/// ```
 pub fn h_5pp(t: f64, n: usize) -> f64 {
 	let t2 = t.powi(2);
 	let t3 = t.powi(3);
